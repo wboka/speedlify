492c65436e254fd6f6cb8281900c4703f49ad570
Wayne Boka <wboka@users.noreply.github.com>
2021-07-08 14:11:13 -0400

    Update and rename sample.js to clients.js

---
+++ _data/sites/clients.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Clients", // optional, falls back to object key
+	description: "Client websites that get tested",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.icebreakershockey.com/"
+	]
+};
